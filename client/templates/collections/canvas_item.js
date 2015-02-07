6fa990079b6acb6da63e2183c0b075ba9a33742c
TBD Labs <tbdlabs@TBDs-MacBook-Air.local>
2015-02-06 19:47:20 -0800

    work in progress. problem with sessions and canvas items.  also basic uploader in place but we need to talk about next steps with it, will need help

--- client/templates/collections/canvas_item.js
+++ client/templates/collections/canvas_item.js
@@ -1,12 +1,38 @@
 Template.canvasItem.events({
 	'click .options-toggle': function(e) {
-		console.log(e.currentTarget.nextElementSibling);
 		var optionsDiv = e.currentTarget.nextElementSibling;
-
-
 		optionsDiv.style.display = (optionsDiv.style.display != 'block' ? 'block' : 'none');
 
+	},
+	'click .remove-canvas-item': function(e) {
+		var thisItem = document.getElementById(this._id);
+		thisItem.parentNode.removeChild(thisItem);
 
+		var obj = Session.get('selectedCollection');
+		delete obj[this._id];
+		Session.set('selectedCollection', obj);
 	}
 
 });
+
+
+Template.canvas.rendered = function() {
+	//this object needs to be a variable that is populated by the id's of the selected collections, then we need to render the canvasItem template for each one
+	
+
+		var p = Session.get('selectedCollection');
+		console.log(p);
+
+		var canvas = document.getElementById('canvas');
+		if(p) {
+			for (var key in p) {
+			  if (p.hasOwnProperty(key)) {
+
+			    var data = Collections.findOne({_id: p[key]});
+
+			    Blaze.renderWithData(Template.canvasItem, data, canvas);
+			  }
+			}
+		}
+
+}
--- client/templates/collections/collections_list.js
+++ client/templates/collections/collections_list.js
@@ -1,25 +1,33 @@
 Template.collectionsList.helpers({
 	'collections': function() {
 		return Collections.find();
-	},
-	'selectedCollection': function() {
-		return !!Session.get('selectedCollections') ? 'selected' : '';
 	}
 
 });
 
 /*Template.collectionsList.created = function() {
-	Session.set('selectedColletions', {});
+	var emnpty_session_obj = {};
+	Session.set('selectedColletion', emnpty_session_obj);
 } */
 
 Template.collectionsList.events({
 	'click li': function(e) {
 		//console.log(this._id);
+
 		var collectionId = this._id;
 		var data = Collections.findOne({_id: this._id});
 		var canvas = document.getElementById('canvas');
 
-		//var selectedItems = {};
+		var selectedItems = Session.get('selectedCollection', selectedItems);
+		if(!selectedItems) {
+			var selectedItems = {};
+		}
+
+		selectedItems[collectionId] = collectionId;
+        Session.set('selectedCollection', selectedItems);
+        console.log(Session.get('selectedCollection'));
+
+		
 		//return Session.set('selectedCollections', selectedItems);
 
 		//var curr = e.currentTarget;
