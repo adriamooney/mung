da731b8b379b7d8d85af996411e87f12b9909dea
JADALM <james.dipadua@gmail.com>
2015-02-08 20:12:04 -0800

    start of csv to json

--- server/fixtures.js
+++ server/fixtures.js
@@ -6,17 +6,17 @@ if (Collections.find().count() === 0) {
     title: 'My DataSet 1',
     file_name: 'some-name.csv',
     date_added: new Date(now - 10 * 3600 * 1000),
-    file_data: {
-        property_0: {
+    file_data: [
+        {
             name: 'height',
             property_data: [100, 50, 60, 75, 120, 101]
         },
-        property_1: {
+        {
             name: 'weight',
             property_data: [120, 150, 160, 175, 220, 201]
         }
-    }
+    ]
   });
 }
 
-console.log(Collections.find().fetch());
+//console.log(Collections.find().fetch());
