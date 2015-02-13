deb3db0c28eb606b263ac384418df6d51af33561
JADALM <james.dipadua@gmail.com>
2015-02-12 16:27:08 -0800

    trying to get csv read to work again with new client integration. issues with passing data around. will ask adria.

--- client/startup.js
+++ client/startup.js
@@ -1,9 +1,12 @@
 Meteor.startup(function () {
 
 //https://github.com/tomitrescak/meteor-uploads/blob/master/client/init.js#L2-L10
-  Uploader.finished = function(file_info) {
-     MungCSV.csv_to_json(file_info);
-     //console.log(csv_data[0]);
+  Uploader.finished = function(index, file_info) {
+      // FOR SOME STUPID FUCKING REASON
+      // UPLOADER.FINISHED PLACES THE FILE INFO AS THE SECOND VARIABLE
+      // IN THIS FILE AND ON THE SERVER/STARTUP.JS IT'S THE FIRST VARIABLE. 
+     var csv_file_data = Meteor.call('convert_CSV', file_info);
+     //console.log(csv_file_data[0]);
 
 /*      var curr = Meteor.userId();
       var user = Meteor.users.findOne({_id:curr});
--- server/collections/methods.js
+++ server/collections/methods.js
@@ -4,5 +4,12 @@ Meteor.methods({
 
       Collections.insert(info);
 
+	}, 
+	convert_CSV: function(file_info){
+		MungCSV.csv_to_json(file_info);
+		console.log(MungCSV);
+//		var csv_data = MungCSV.csv_data;
+//		console.log(csv_data);
+		//return file_data;
 	}
 });
