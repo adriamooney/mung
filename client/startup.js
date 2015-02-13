af666d6ef9497e049f945adc30531acfc7d4d5a5
JADALM <james.dipadua@gmail.com>
2015-02-12 22:13:56 -0800

    think got the file stuff working now. doh

--- client/startup.js
+++ client/startup.js
@@ -5,28 +5,20 @@ Meteor.startup(function () {
       // FOR SOME STUPID FUCKING REASON
       // UPLOADER.FINISHED PLACES THE FILE INFO AS THE SECOND VARIABLE
       // IN THIS FILE AND ON THE SERVER/STARTUP.JS IT'S THE FIRST VARIABLE. 
-     var csv_file_data = Meteor.call('convert_CSV', file_info);
-     //console.log(csv_file_data[0]);
 
+      file_info.user_id = Meteor.userId();
+
+      Meteor.call('convert_CSV', file_info);
+ 
 /*      var curr = Meteor.userId();
-      var user = Meteor.users.findOne({_id:curr});
+      
 
-      var now = new Date().getTime();
+
 
       //TODO: put actual data in the `file_data`. and `file_name`.  
       //Allow user to set `title` before upload, then set that title in a session and it can then be entered in here:
 
-      var info = {
-        title: 'new data set',
-        file_name: 'some-name.csv',
-        uploadedBy: user._id,
-        orgName: user.orgName,
-        orgId: user.orgId,
-        date_added: new Date(now - 10 * 3600 * 1000),
-        file_data: 'data'
-      }
-
-      Meteor.call('addNewCollection', info);
+      
 */
 
   }
