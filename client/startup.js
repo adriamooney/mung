194b2e42467fe5c4dcb349788beb7c28754fb708
JADALM <james.dipadua@gmail.com>
2015-02-13 20:36:01 -0800

    insertion working now. needed to wrap parser callback function with meteor.bindEnvironment. whew. fucking a.

--- client/startup.js
+++ client/startup.js
@@ -6,71 +6,15 @@ Meteor.startup(function () {
       // UPLOADER.FINISHED PLACES THE FILE INFO AS THE SECOND VARIABLE
       // IN THIS FILE AND ON THE SERVER/STARTUP.JS IT'S THE FIRST VARIABLE. 
 
-      file_info.user_id = Meteor.userId();
-
-      Meteor.call('convert_CSV', file_info);
- 
-/*      var curr = Meteor.userId();
-      
-
-
-
-      //TODO: put actual data in the `file_data`. and `file_name`.  
-      //Allow user to set `title` before upload, then set that title in a session and it can then be entered in here:
-
-      
-*/
+    var user_id = Meteor.userId();
+    var user = Meteor.users.findOne({_id: user_id});
+	file_info.user_id = user_id;
+	file_info.org_id = user.orgId;
+	file_info.org_name = user.orgName;
+	file_info.dt_added = new Date();
+	
+    Meteor.call('convert_CSV', file_info);
 
   }
 
 });
-
-
-
-//http://docs.meteor.com/#/full/tracker_autorun
-//For example, you can monitor a cursor (which is a reactive data source) 
-//and aggregate it into a session variable:
-
-/*Tracker.autorun(function () {
-  var oldest = _.max(Monkeys.find().fetch(), function (monkey) {
-    return monkey.age;
-  });
-  if (oldest)
-    Session.set("oldest", oldest.name);
-}); */
-
-var tomId = Meteor.users.insert({
-        emails: [
-        // each email address can only belong to one user.
-            { address: "adriamooney+mung@gmail.com", verified: true },
-        ],
-        profile: { 
-            name: 'Adria Mooney',
-            orgName: org.name,
-            orgId: org._id,
-            accountCode: org.accountCode,
-            accountStatus: 'active'
-        },
-        roles: ['superadmin']
-    });
-
-
-
-Collections.insert({
-        title: 'Data set with a really long name about stuff',
-        file_name: 'some-name.csv',
-        uploadedBy: tom._id,
-        orgName: org.name,
-        orgId: org._id,
-        date_added: new Date(now - 10 * 3600 * 1000),
-        file_data: [
-            {
-                name: 'height',
-                property_data: [100, 50, 60, 75, 120, 101]
-            },
-            {
-                name: 'weight',
-                property_data: [120, 150, 160, 175, 220, 201]
-            }
-        ]
-    });
