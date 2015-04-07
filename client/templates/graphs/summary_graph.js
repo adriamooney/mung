8a5e1c74ec3eda00f86d4c71e5c3c4cf898aa21d
JADALM <james.dipadua@gmail.com>
2015-04-07 16:23:54 -0700

    fully functional bargraphs. hooray

--- client/templates/graphs/summary_graph.js
+++ client/templates/graphs/summary_graph.js
@@ -5,13 +5,13 @@ Template.summaryGraph.helpers({
   graphId: function() {
     var graph_data = Session.get('summaryGraph');
     var last = graph_data.length -1;
-    console.log(graph_data);
-    console.log(graph_data[last][0]);
+    //console.log(graph_data);
+    //console.log(graph_data[last][0]);
     return graph_data[last][0]; //THIS IS NOT WORKING
   },
   chart: function() {
   	var graph_data = Session.get('summaryGraph');
-    console.log(graph_data);
+    //console.log(graph_data);
   	var graphs = [];
   	for (i = 1; i< graph_data.length; i++) {
       graphs.push[createSvg(graph_data[i])];
@@ -22,19 +22,16 @@ Template.summaryGraph.helpers({
 
 
 function createSvg(data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      //console.log(data[i].values);
-      if (data[i].values[0].value) {
-        var chart = nv.models.discreteBarChart();
-                // .x(function(d) { return d.label })
-                // .y(function(d) { return d.value })
-                // .staggerLabels(true)
-                // .tooltips(false)
-                // .showValues(true)
+      // first figure out what kind of graph we're making here... 
+      if (data[0].values[0].value) {
+        var chart = nv.models.discreteBarChart()
+                 .x(function(d) { return d.label })
+                 .y(function(d) { return d.value })
+                 .staggerLabels(false)
+                 .tooltips(false)
+                 .showValues(true)
                 ;
-
-      }else {
+      } else {
         var chart = nv.models.lineChart()
                 .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
                 //.useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
@@ -43,12 +40,13 @@ function createSvg(data) {
                 .showXAxis(true)        //Show the x-axis 
                 ;
       }
-
-    }
-    
+      console.log(chart);
     nv.addGraph(function() {
-        chart.xAxis.tickFormat(d3.format('f'));
-        chart.yAxis.axisLabel(data.key).tickFormat(d3.format(',g'));
+        // THIS IS USED A LOT IN EXAMPLES
+        // BUT FOR ME, IT WAS CAUSING THE BARGRAPHS TO BREAK
+        // COMMENTING IT OUT DIDN'T SEEM TO AFFECT GRAPH CREATION SO FUCK IT. 
+        //chart.xAxis.tickFormat(d3.format('f'));
+        //chart.yAxis.axisLabel(data.key).tickFormat(d3.format('g'));  
         svg = d3.select('#summary-graph').append('div').attr("class", "svg-wrap").html('<button class="btn label label-danger remove-svg-item"><i class="fa fa-times"></i> Remove</button>').append('svg');
         
         var svgAttributes = {
