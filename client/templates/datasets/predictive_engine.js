150b72ab00bf270d892dce307a21cd8ee6bf1a72
JADALM <james.dipadua@gmail.com>
2015-04-10 20:13:23 -0700

    ready to start writing logistic regression part now

--- client/templates/datasets/predictive_engine.js
+++ client/templates/datasets/predictive_engine.js
@@ -4,7 +4,9 @@ Template.choosePredictiveEngine.events({
 		if(engine == 'logisticReg') {
 			var iterations = template.find(".gradient_iterations option:selected").value;
 			var logistic_regression = MungLogisticRegression.init(this._id, iterations);
-
+			// after getting the logistic regression response
+			// set it to a session and instatiate a new template that presents an input
+			// then click to get a prediction
 		}// end logistic regression
 	}
 
