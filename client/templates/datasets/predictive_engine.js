3c4bdef4d2f311ed0020725d2df9b075675603ab
JADALM <james.dipadua@gmail.com>
2015-04-16 21:00:19 -0700

    trying to get linear regression going (as a break from log-reg). fucking stuck. FML

--- client/templates/datasets/predictive_engine.js
+++ client/templates/datasets/predictive_engine.js
@@ -1,13 +1,16 @@
 Template.choosePredictiveEngine.events({
 	'click #predictive-engine-choice': function (event, template) {
 		var engine = template.find(".choosePredictiveEngine input[type='radio']:checked").value;
+		var iterations = template.find(".gradient_iterations option:selected").value;
 		if(engine == 'logisticReg') {
-			var iterations = template.find(".gradient_iterations option:selected").value;
 			var logistic_regression = MungLogisticRegression.init(this._id, iterations);
 			// after getting the logistic regression response
 			// set it to a session and instatiate a new template that presents an input
 			// then click to get a prediction
 		}// end logistic regression
+		else if (engine == 'linearReg') {
+			var logistic_regression = MungLinearRegression.init(this._id, iterations);
+		}
 	}
 
 });
