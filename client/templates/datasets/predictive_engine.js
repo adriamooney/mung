f13ef7accc9842cc620286df6d62441fe6542cd3
JADALM <james.dipadua@gmail.com>
2015-04-20 15:44:52 -0700

    pushing just in case i have not for a while

--- client/templates/datasets/predictive_engine.js
+++ client/templates/datasets/predictive_engine.js
@@ -1,14 +1,16 @@
 Template.choosePredictiveEngine.events({
 	'click #predictive-engine-choice': function (event, template) {
 		var engine = template.find(".choosePredictiveEngine input[type='radio']:checked").value;
-		var iterations = template.find(".gradient_iterations option:selected").value;
 		if(engine == 'logisticReg') {
+			console.log(this);
+			var iterations = template.find(".logistic_iterations_"+this._id+" option:selected").value;
 			var logistic_regression = MungLogisticRegression.init(this._id, iterations);
 			// after getting the logistic regression response
 			// set it to a session and instatiate a new template that presents an input
 			// then click to get a prediction
 		}// end logistic regression
 		else if (engine == 'linearReg') {
+			var iterations = template.find(".linear_iterations_"+this._id+" option:selected").value;
 			var logistic_regression = MungLinearRegression.init(this._id, iterations);
 		}
 	}
